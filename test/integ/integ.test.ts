3bf7a5ddfe0cd1a9c4859910ee6d71769dc8a3d8
mfreema1 <mfreema1@stevens.edu>
2019-10-09 18:44:57 -0400

    Refactoring / updating tests and access layer

--- test/integ/integ.test.ts
+++ test/integ/integ.test.ts
@@ -1,6 +1,3 @@
-/**
- * Run integration tests against a local instance of the express server
- */
 import GatewayRunner from './local';
 
 const runner: GatewayRunner = new GatewayRunner();
@@ -9,9 +6,9 @@ afterAll(() => {
     runner.close();
 })
 
-//FIXME:  Any seed scripts which are relied on for this
-//must be called from this module.  Must be self-contained
-//for Travis CI testing
+// FIXME:  Any seed scripts which are relied on for this
+// must be called from this module.  Must be self-contained
+// for Travis CI testing
 test('Run a sample test', async () => {
     const response: any = await runner.runEvent({
         httpMethod: 'GET',
@@ -23,7 +20,6 @@ test('Run a sample test', async () => {
         }
     });
 
-    console.log(response);
     expect(response.statusCode).toBe(200);
 })
 
@@ -33,35 +29,32 @@ test('Provide a request with bad query params', async () => {
         body: '',
         path: '/projects',
         queryStringParameters: {
-            sort_by: 'foo' //not a valid sort parameter
+            sort_by: 'foo' // invalid sort parameter
         }
     })
 
-    console.log(response);
     expect(response.statusCode).toBe(400);
 })
 
 test('Provide a bad reqest', async () => {
     const response: any = await runner.runEvent({
         httpMethod: 'POST',
-        body: '', //invalid body
+        body: '', // invalid body
         path: '/projects',
         queryStringParameters: {}
     })
 
-    console.log(response);
     expect(response.statusCode).toBe(400);
 })
 
-test('Get a specific project', async () => {
+test('Get a specific project which does not exist', async () => {
     const response: any = await runner.runEvent({
         httpMethod: 'GET',
         body: '',
-        path: '/projects/32697a46-8d7b-4b43-8fef-4ea38cd2ec71',
+        path: '/projects/ffffffff-ffff-ffff-ffff-ffffffffffff',
         queryStringParameters: {},
         requestContext: {}
     })
 
-    console.log(response);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(404);
 })
