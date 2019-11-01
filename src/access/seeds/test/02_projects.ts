2a824aa2af5a6c157f7de00a805b735808a63bd7
mfreema1 <mfreema1@stevens.edu>
2019-11-01 10:40:44 -0400

    Include body field in db

--- src/access/seeds/test/02_projects.ts
+++ src/access/seeds/test/02_projects.ts
@@ -12,7 +12,8 @@ export async function seed(knex: Knex): Promise<any> {
 				return {
 					id: uuid(),
 					title: faker.company.companyName(),
-					tagline: faker.company.catchPhrase(),
+                    tagline: faker.company.catchPhrase(),
+                    body: faker.lorem.paragraphs(2),
 					accepting_applications: Math.floor(Math.random() * 2),
 					created_at: faker.date.past(10) //up to 10 years ago
 				};
@@ -22,21 +23,24 @@ export async function seed(knex: Knex): Promise<any> {
 				{
 					id: "5a4fe8c2-02c7-4596-a4a7-b7788defb8e1",
 					title: "The Matrix",
-					tagline: "Be Young, Have Fun, Taste Ooga Booga.",
+                    tagline: "Be Young, Have Fun, Taste Ooga Booga.",
+                    body: faker.lorem.paragraphs(2),
 					accepting_applications: false,
 					created_at: faker.date.past(10)
 				},
 				{
 					id: "25510168-bd7a-446e-b559-38ff9998bdb4",
 					title: "Blade Runner",
-					tagline: "Now With 50% More Ooga Booga!",
+                    tagline: "Now With 50% More Ooga Booga!",
+                    body: faker.lorem.paragraphs(2),
 					accepting_applications: true,
 					created_at: faker.date.past(10)
 				},
 				{
 					id: "aff84430-de88-4c5d-8072-ad1999a01850",
 					title: "The Godfather",
-					tagline: "Oh Hungry? Oh Ooga Booga.",
+                    tagline: "Oh Hungry? Oh Ooga Booga.",
+                    body: faker.lorem.paragraphs(2),
 					accepting_applications: false,
 					created_at: faker.date.past(10)
 				},
