3ea81f959f2bcb52e7865a292098ccea6923d72e
mfreema1 <mfreema1@stevens.edu>
2020-03-23 23:18:43 -0400

    Remove extraneous fields from project shared model

--- lib/types/shared/ProjectShared.ts
+++ lib/types/shared/ProjectShared.ts
@@ -1,9 +1,3 @@
-import { BoardEntryShared } from "./BoardEntryShared";
-import { ApplicationShared } from "./ApplicationShared";
-import { UserShared } from "./UserShared";
-import { TagShared } from "./TagShared";
-import { MajorShared } from "./MajorShared";
-
 export interface ProjectShared {
 
     /**
@@ -51,46 +45,4 @@ export interface ProjectShared {
      * @maxLength 256
      */
     thumbnailLink: string;
-
-    // /**
-    //  * Entries on the project board of a project
-    //  * @minItems 0
-    //  */
-    // boardItems: BoardEntryShared[];
-
-    // /**
-    //  * Applications to the project
-    //  * @minItems 0
-    //  */
-    // applications: ApplicationShared[];
-
-    // /**
-    //  * People who are working on the project
-    //  * @minItems 0
-    //  */
-    // contributors: UserShared[];
-
-    // /**
-    //  * Contributors with elevated privileges
-    //  * @minItems 0
-    //  */
-    // administrators: UserShared[];
-
-    // /**
-    //  * Tags associated with the project
-    //  * @minItems 0
-    //  */
-    // tags: TagShared[];
-
-    // /**
-    //  * Majors of students that are needed for the project
-    //  * @minItems 0
-    //  */
-    // requestedMajors: MajorShared[];
-
-    // /**
-    //  * Users that have starred this project
-    //  * @minItems 0
-    //  */
-    // starredBy: UserShared[];
 }
